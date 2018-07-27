97357171e99242414c92a9fc714454cb60262015
root <sam.sodhi143@hotmail.com>
2018-07-27 20:37:40 +0530

    go

--- circleci/edit.sh
+++ circleci/edit.sh
@@ -6,6 +6,6 @@ echo "[default]" >> ~/.aws/config
 echo "region = ap-south-1" >> ~/.aws/config
 echo "output = json" >> ~/.aws/config
 
-echos "[default]" >> ~/.aws/credentials
+echo "[default]" >> ~/.aws/credentials
 echo "aws_secret_access_key = " $AWS_SECRET_ACCESS_KEY >> ~/.aws/credentials
 echo "aws_access_key_id = " $AWS_ACCESS_KEY_ID >> ~/.aws/credentials
